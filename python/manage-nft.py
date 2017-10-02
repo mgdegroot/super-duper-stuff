68a46f2e534877cfe5b678e2e808e4b0f9f4d3de
Marcel de Groot <mg.degroot@catnipderby.nl>
2017-10-02 19:48:58 +0200

    small cleanup

--- python/manage-nft.py
+++ python/manage-nft.py
@@ -2,6 +2,21 @@
 import os
 import requests
 import subprocess
+import argparse
+import pprint
+from enum import Enum
+from urllib.parse import urlparse
+
+
+def load_src(src:str):
+    url = urlparse(src)
+
+    if url.scheme is not None and url.scheme != "":
+        raw_list = load_from_url(src)
+    else:
+        raw_list = load_from_file(src)
+
+    return raw_list
 
 
 def load_from_file(full_path:str):
@@ -15,7 +30,7 @@ def load_from_file(full_path:str):
     return raw_list
 
 
-def load_from_url(url):
+def load_from_url(url:str):
     response = requests.get(url)
     response.raise_for_status()
 
@@ -68,22 +83,93 @@ def nft_add_set_to_chain(set_name:str, table:str, chain:str):
         raise ChildProcessError("no success....")
 
 
+def parse_args():
+    action_choices = ["status", "refresh", "load", "start", "stop", "just-do-it-already"]
+
+    user_opts = {
+        "ACTION": action_choices[0] ,
+        "SRC": "/home/blaatenator/test/emerging-Block-IPs.txt",
+        # "SRC": "http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt",
+        "TABLE": "inet filter",
+        "CHAIN": "input",
+        "SET_NAME": "wan_blocks",
+    }
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("action", choices=action_choices, type=str,
+                        help="What to do")
+    parser.add_argument("--src", default=user_opts["SRC"], type=str,
+                        help="Where to fetch the blocklist from")
+    parser.add_argument('--table', default=user_opts["TABLE"], type=str,
+                        help="What NF table to use")
+    parser.add_argument('--chain', default=user_opts["CHAIN"], type=str,
+                        help="What chain to use")
+    parser.add_argument("--set-name", dest="set_name", default=user_opts["SET_NAME"], type=str,
+                        help="The name of the ip / network set")
+
+    args = parser.parse_args()
+
+    if args.action:
+        user_opts["ACTION"] = args.action
+    if args.src:
+        user_opts["SRC"] = args.src
+    if args.table:
+        user_opts["TABLE"] = args.table
+    if args.chain:
+        user_opts["CHAIN"] = args.chain
+    if args.set_name:
+        user_opts["SET_NAME"] = args.set_name
+
+    return user_opts
+
+
+class Action(Enum):
+    REFRESH = 1,
+    STATUS = 2
+
+
 def main():
-    URL_RULES = 'http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt'
-    URL_RULES_REVISION = 'http://rules.emergingthreats.net/fwrules/FWrev'
-    SET_NAME="wan_blocks"
-    TABLE_NAME="inet filter"
-    CHAIN_NAME="input"
+    # URL_RULES = "http://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt"
+    URL_RULES_REVISION = "http://rules.emergingthreats.net/fwrules/FWrev"
+    # SET_NAME="wan_blocks"
+    # TABLE_NAME="inet filter"
+    # CHAIN_NAME="input"
 
-    src_filename = os.path.expanduser('/home/blaatenator/test/emerging-Block-IPs.txt')
+    user_opts = parse_args()
+    steps_to_perform = []
 
-    raw_list = load_from_file(src_filename)
-    # raw_list = load_from_url(URL_RULES)
+    if user_opts["ACTION"] == "just-do-it-already":
+        # steps_to_perform = [1,2,3,4,5]
+        raw_list = load_src(user_opts["SRC"])
+        content_list = filter_raw(raw_list)
 
-    content_list = filter_raw(raw_list)
-    nft_create_set(set_name=SET_NAME, table=TABLE_NAME)
-    nft_add_to_set(set_name=SET_NAME, table=TABLE_NAME, content_list=content_list)
-    nft_add_set_to_chain(set_name=SET_NAME, table=TABLE_NAME, chain=CHAIN_NAME)
+        nft_create_set(set_name=user_opts["SET_NAME"], table=user_opts["TABLE"])
+        nft_add_to_set(set_name=user_opts["SET_NAME"], table=user_opts["TABLE"], content_list=content_list)
+        nft_add_set_to_chain(set_name=user_opts["SET_NAME"], table=user_opts["TABLE"], chain=user_opts["CHAIN"])
+    if user_opts["ACTION"] == "load":
+        # fetch list, parse, load in nftables but do not activate (reference) in chain yet
+        raw_list = load_src(user_opts["SRC"])
+        content_list = filter_raw(raw_list)
+
+        nft_create_set(set_name=user_opts["SET_NAME"], table=user_opts["TABLE"])
+        nft_add_to_set(set_name=user_opts["SET_NAME"], table=user_opts["TABLE"], content_list=content_list)
+
+    elif user_opts["ACTION"] == "start":
+        # set already loaded in nftables
+        # reference the set in a table-chain
+        nft_add_set_to_chain(set_name=user_opts["SET_NAME"], table=user_opts["TABLE"], chain=user_opts["CHAIN"])
+
+
+    # if 1 in steps_to_perform:
+    #
+
+    # src_filename = os.path.expanduser('/home/blaatenator/test/emerging-Block-IPs.txt')
+    #
+    # raw_list = load_from_file(src_filename)
+    # # raw_list = load_from_url(URL_RULES)
+    #
+    # content_list = filter_raw(raw_list)
 
 
 if __name__ == '__main__':
